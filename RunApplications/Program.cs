658e1336ce2a1aa2c724db0a479e32c57cef6316
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 22:28:37 +0300

    Add project files.

---
+++ RunApplications/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("https://drive.google.com/drive/search?q=html");
+        }
+    }
+}
